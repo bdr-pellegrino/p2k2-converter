8b4b05546d1790efbe9306792274dd3ab56cf9d7
Giovanni Antonioni <giovanni.antonioni2@studio.unibo.it>
2024-01-16 11:26:04 +0100

    chore(init): initialize project

---
+++ p2k2_converter/__main__.py
@@ -0,0 +1,7 @@
+import p2k2_converter
+
+# this is the main module of your app
+# it is only required if your project must be runnable
+# this is the script to be executed whenever some users writes `python -m p2k2_converter` on the command line, eg.
+x = p2k2_converter.MyClass().my_method()
+print(x)
--- test/__init__.py
+++ test/__init__.py
@@ -1,5 +1,5 @@
 import unittest
-from my_project import MyClass
+from p2k2_converter import MyClass
 
 class TestMyClass(unittest.TestCase):
     # test methods' names should begin with `test_`
