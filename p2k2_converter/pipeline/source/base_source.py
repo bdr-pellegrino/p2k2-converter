d751339c3e69c2c53804833b3f06013c48fe13a5
Giovanni Antonioni <giovanni.antonioni2@studio.unibo.it>
2024-01-17 16:28:49 +0100

    refactor: now base classes are defined as abstract

--- p2k2_converter/pipeline/source/base_source.py
+++ p2k2_converter/pipeline/source/base_source.py
@@ -1,10 +1,12 @@
 from typing import TypeVar
 from contextlib import contextmanager
+from abc import ABC, abstractmethod
 
 T = TypeVar('T')
 
 
-class BaseSource:
+class BaseSource(ABC):
     @contextmanager
+    @abstractmethod
     def open(self) -> T:
         pass
--- p2k2_converter/pipeline/step/base_step.py
+++ p2k2_converter/pipeline/step/base_step.py
@@ -1,14 +1,15 @@
 from typing import TypeVar
-
+from abc import ABC, abstractmethod
 T = TypeVar('T')
 
 
-class BaseStep:
+class BaseStep(ABC):
     def __init__(self, name):
         self.__name = name
 
     def name(self):
         return self.__name
 
+    @abstractmethod
     def execute(self) -> T:
         pass
