9ee1f9086321513da7500a3644bd9f4712637da3
Giovanni Antonioni <giovanni.antonioni2@studio.unibo.it>
2024-01-17 16:51:30 +0100

    feature: add base implementation for data_mapper.py class

--- p2k2_converter/pipeline/step/data_extractor.py
+++ p2k2_converter/pipeline/step/data_extractor.py
@@ -1,21 +1,22 @@
-from .base_step import BaseStep
+from abc import ABC, abstractmethod
+from .base_step import BaseStep, T
 
 
-class DataExtractor(BaseStep):
+class DataExtractor(BaseStep, ABC):
     def __init__(self, name: str, source=None):
         super().__init__(name)
         self.__source = source
 
     def set_data(self, source):
-        """
-        Sets the source to be used in this step
-        """
         self.__source = source
 
-    def extract_data(self):
-        """
-        Extracts data from the source
-        """
+    def execute(self) -> T:
+        if self.__source is None:
+            raise ValueError("No source set for this step")
+        return self.extract_data(self.__source)
+
+    @abstractmethod
+    def extract_data(self, source) -> T:
         pass
 
 
---
+++ p2k2_converter/pipeline/step/data_mapper.py
@@ -0,0 +1,20 @@
+from abc import ABC, abstractmethod
+from .base_step import BaseStep, T
+
+
+class DataExtractor(BaseStep, ABC):
+    def __init__(self, name: str, data=None):
+        super().__init__(name)
+        self.__data = data
+
+    def set_data(self, source):
+        self.__data = source
+
+    def execute(self) -> T:
+        if self.__data is None:
+            raise ValueError("No data provided for this step")
+        return self.extract_data(self.__data)
+
+    @abstractmethod
+    def extract_data(self, data) -> T:
+        pass
