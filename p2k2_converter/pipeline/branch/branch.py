748112ec0c6715f35458115ce2b0386f00d88aa0
Giovanni Antonioni <giovanni.antonioni2@studio.unibo.it>
2024-01-18 15:14:05 +0100

    refactor: add type support for branch class

--- p2k2_converter/pipeline/branch/branch.py
+++ p2k2_converter/pipeline/branch/branch.py
@@ -1,5 +1,10 @@
 from copy import deepcopy as copy
+
+from p2k2_converter.pipeline.source import BaseSource
 from p2k2_converter.pipeline.step import BaseStep, DataExtractor
+from typing import TypeVar
+
+T = TypeVar("T")
 
 
 class Branch:
@@ -19,7 +24,7 @@ class Branch:
         self.__name = name
         self.__executed = False
 
-    def add_step(self, step: BaseStep, force=False) -> None:
+    def add_step(self, step: BaseStep, force: bool = False) -> None:
         if isinstance(step, DataExtractor):
             if self.__steps["extractor"] is not None and not force:
                 raise ValueError("DataExtractor already set for this branch")
@@ -29,13 +34,13 @@ class Branch:
                 raise ValueError("DataMapper already set for this branch")
             self.__steps["mappers"][step.get_name()] = step
 
-    def get_name(self):
+    def get_name(self) -> str:
         return self.__name
 
-    def get_steps(self):
+    def get_steps(self) -> dict[str, BaseStep]:
         return copy(self.__steps)
 
-    def execute(self, source):
+    def execute(self, source: BaseSource) -> T:
         if self.__executed:
             raise RuntimeError("Branch already executed")
 
