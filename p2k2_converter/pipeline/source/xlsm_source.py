fad6668139160e97926417194ad51b0f7c431371
Giovanni Antonioni <giovanni.antonioni2@studio.unibo.it>
2024-02-06 16:02:52 +0100

    refactor: xlsm_source handle error

--- p2k2_converter/pipeline/source/xlsm_source.py
+++ p2k2_converter/pipeline/source/xlsm_source.py
@@ -1,6 +1,7 @@
 from contextlib import contextmanager
 from openpyxl import load_workbook
 from openpyxl.workbook import Workbook
+from openpyxl.utils.exceptions import InvalidFileException
 from .base_source import BaseSource
 
 
@@ -12,6 +13,14 @@ class XlsmSource(BaseSource):
 
     @contextmanager
     def open(self) -> Workbook:
-        wb = load_workbook(self.__path, read_only=True)
-        yield wb
-        wb.close()
+        try:
+            wb = load_workbook(self.__path, read_only=True)
+            yield wb
+            wb.close()
+        except FileNotFoundError:
+            print(f"Error: File {self.__path} not found.")
+            exit(1)
+        except InvalidFileException:
+            print(f"Error: File {self.__path} is not a valid xlsm file.")
+            exit(1)
+
