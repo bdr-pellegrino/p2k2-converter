3d29341884ec7e5672ee5f2797ab9016845f6afb
Giovanni Antonioni <giovanni.antonioni2@studio.unibo.it>
2024-02-22 12:03:53 +0100

    ci: disable @semantic-release/exec

--- release.config.js
+++ release.config.js
@@ -12,12 +12,12 @@ if (dryRun) {
 var config = require('semantic-release-preconfigured-conventional-commits');
 
 config.plugins.push(
-    [
+    /*[
         "@semantic-release/exec",
         {
             "publishCmd": publishCmd,
         }
-    ],
+    ],*/
     ["@semantic-release/github", {
         "assets": [ 
             { "path": "dist/*" },
