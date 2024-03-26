775ac32c616582ae397442004df45501708d3ade
Giovanni Antonioni <giovanni.antonioni2@studio.unibo.it>
2024-03-26 17:57:50 +0100

    chore: prepare test files for moderna files

---
+++ p2k2_converter/core/workflow/moderna.py
@@ -0,0 +1,18 @@
+from p2k2_converter.core.workflow import WorkflowStrategy
+
+
+class Moderna(WorkflowStrategy):
+    def model_definition(self, source, data):
+        pass
+
+    def profiles_definition(self, source, model):
+        pass
+
+    def cuts_definition(self, source, model):
+        pass
+
+    def machining_definition(self, source, model):
+        pass
+
+    def translation_definition(self, source, model):
+        pass
