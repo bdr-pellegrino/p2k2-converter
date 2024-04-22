a861ab3f62de546b6dec0e270c75fc63f7c89dcd
Giovanni Antonioni <giovanni.antonioni2@studio.unibo.it>
2024-04-22 16:56:22 +0200

    feat: implement translation_definition for Modx class

--- p2k2_converter/core/workflow/modx.py
+++ p2k2_converter/core/workflow/modx.py
@@ -3,9 +3,49 @@ from openpyxl.workbook import Workbook
 
 from p2k2_converter.core.classes import Model
 from p2k2_converter.core.workflow import Workflow
+from p2k2_converter.p2k2 import CutBuilder
 
 
 class Modx(Workflow, ABC):
 
     def translation_definition(self, workbook, model) -> [Workbook, Model]:
-        pass
+        output = {}
+
+        profile_code = "PROFILO DOGA"
+        profile = model.profiles[profile_code]
+        cuts = profile.cuts
+
+        # Create a list of CutBuilder with the information of the cuts
+        builders = [
+            CutBuilder().add_cut_length(cut.length)
+            .add_left_cutting_angle(cut.angleL)
+            .add_right_cutting_angle(cut.angleR)
+            for cut in cuts
+        ]
+
+        builders = self.apply_labels(builders, workbook)
+        output[profile_code] = [builder.build() for builder in builders]
+
+        profile_code = "TAGLIO GUIDA"
+        profile = model.profiles[profile_code]
+        cuts = profile.cuts
+        machinings = profile.machinings
+
+        model_height = model.height
+        profile_code_1 = f"{machinings[0]} {model_height}"
+        profile_code_2 = f"{machinings[1]} {model_height}"
+
+        builders = [
+            CutBuilder().add_cut_length(cut.length)
+            .add_left_cutting_angle(cut.angleL)
+            .add_right_cutting_angle(cut.angleR)
+            for cut in cuts
+        ]
+
+        builders[0].add_machining(profile_code_1, 1)
+        builders[1].add_machining(profile_code_2, 1)
+
+        output[profile_code] = [builder.build() for builder in builders]
+
+        return output
+
